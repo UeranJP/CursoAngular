3553674256c315ad11e7717e9977d8068d0fcfed
UeranJP <ueran.piazza@gmail.com>
2020-02-07 08:37:09 -0300

    Primeiro Commit

---
+++ src/app/order/order.service.ts
@@ -0,0 +1,44 @@
+import { MEAT_API } from './../app.api';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import { CartItem } from './../restaurant-detail/shopping-cart/cart-item.model';
+import { ShoppingCartService } from './../restaurant-detail/shopping-cart/shopping-cart.service';
+import { Injectable } from '@angular/core';
+import { Order, OrderItem } from './order.model';
+import 'rxjs/add/operator/map'
+
+@Injectable()
+export class OrderSerivce{
+
+  constructor(private cartService: ShoppingCartService, private http: HttpClient){ }
+
+  itemsValue(): number{
+    return this.cartService.total()
+  }
+
+  cartItems(): CartItem[]{
+    return this.cartService.items
+  }
+
+  increaseQty(item: CartItem){
+    this.cartService.increaseQty(item)
+  }
+
+  decreaseQty(item: CartItem){
+    this.cartService.decreaseQty(item)
+  }
+
+  remove(item: CartItem){
+    this.cartService.removeItem(item)
+  }
+
+  checkOrder(order: Order): Observable<string> { // <Order>
+
+    return this.http.post<Order>(`${MEAT_API}/orders`, order)
+        .map(order => order.id)
+  }
+
+  clear(){
+    this.cartService.clear()
+  }
+}
